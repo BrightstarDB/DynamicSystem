93a8b94ffda8a811fe115c136c138a9934ed881c
Graham Moore <gra@networkedplanet.com>
2012-07-18 15:10:13 +0200

    First upload of dynamic system src

---
+++ BrightstarDB.DynamicSystem.Tests/SystemTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BrightstarDB.DynamicSystem.Tests
+{
+    [TestClass]
+    public class SystemTests
+    {
+        private const string ConnectionString = "type=embedded;storesdirectory=c:\\brightstar";
+
+        [TestMethod]
+        public void TestCreateObjectSystem()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            Assert.IsNotNull(dos);
+        }
+
+        [TestMethod]
+        public void TestNoTypesExist()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            var typeCount = dos.Types.Count();
+            Assert.AreEqual(0, typeCount);
+        }
+
+        [TestMethod]
+        public void TestAssertType()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            var typeCount = dos.Types.Count();
+            Assert.AreEqual(0, typeCount);
+
+            dos.AssertType("Person", new string[] { "age", "name", "hometown" });
+
+            typeCount = dos.Types.Count();
+            Assert.AreEqual(1, typeCount);
+        }
+
+        [TestMethod]
+        public void TestGetType()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            var typeCount = dos.Types.Count();
+            Assert.AreEqual(0, typeCount);
+            dos.AssertType("Person", new string[] { "age", "name", "hometown" });
+            var type = dos.GetType("Person");
+        }
+
+        [TestMethod]
+        public void TestEnumerateTypeAllowedProperties()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            var typeCount = dos.Types.Count();
+            Assert.AreEqual(0, typeCount);
+            dos.AssertType("Person", new string[] { "age", "name", "hometown" });
+            var type = dos.GetType("Person");
+
+            var properties = type.ds__properties as IEnumerable<object>;
+            Assert.AreEqual(3, properties.ToList().Count());
+
+            Assert.IsTrue(properties.Contains("age"));
+            Assert.IsTrue(properties.Contains("name"));
+            Assert.IsTrue(properties.Contains("hometown"));
+        }
+
+        [TestMethod]
+        public void TestCreateUntypedDynamic()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            var dyna1 = dos.CreateNewDynamicObject();
+            dos.SaveChanges();
+            Assert.IsNotNull(dyna1.Id);
+        }
+
+        [TestMethod]
+        public void TestCreateTypedDynamic()
+        {
+            var storeId = Guid.NewGuid().ToString();
+            var dos = new DynamicObjectSystem(ConnectionString, storeId);
+            var person = dos.AssertType("Person", new string[] { "age", "name", "hometown" });
+            var bob = dos.CreateNewDynamicObject("Person");
+            Assert.AreEqual(person.Id.FirstOrDefault(), bob.ds__type.FirstOrDefault().Id.FirstOrDefault());
+        }
+    }
+}
---
+++ BrightstarDB.DynamicSystem/DynamicObjectSystem.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BrightstarDB.Client;
+using BrightstarDB.Dynamic;
+
+namespace BrightstarDB.DynamicSystem
+{
+    /// <summary>
+    /// The dynamic object system allows for the creation and persistence of types and instances.
+    /// Both types and instances are data driven.
+    /// </summary>
+    public class DynamicObjectSystem
+    {
+        private readonly string _connectionString;
+        private readonly string _storeId;
+        private readonly DynamicStore _dynamicStore;
+        private const string UriPrefix = "http://www.brightstardb.com/dynamic/system/";
+
+        public DynamicObjectSystem(string connectionString, string storeId)
+        {
+            _connectionString = connectionString;
+            _storeId = storeId;
+            var dataObjectContext = BrightstarService.GetDataObjectContext(_connectionString);
+            var dynamicContext = new BrightstarDynamicContext(dataObjectContext);
+            if (dynamicContext.DoesStoreExist(_storeId))
+            {
+                _dynamicStore = dynamicContext.OpenStore(_storeId, new Dictionary<string, string>() { { "ds", UriPrefix } });
+            }
+            else
+            {
+                _dynamicStore = dynamicContext.CreateStore(_storeId, new Dictionary<string, string>() { { "ds", UriPrefix } });
+            }                
+        }
+
+        private const string TypesQuery = @"select ?id where { ?id <http://www.brightstardb.com/dynamic/system/type> ""type""^^<http://www.w3.org/2001/XMLSchema#string> }";
+        private const string GetTypeByName = @"select ?id where {{ ?id <http://www.brightstardb.com/dynamic/system/name> ""{0}""^^<http://www.w3.org/2001/XMLSchema#string> . ?id <http://www.brightstardb.com/dynamic/system/type> ""type""^^<http://www.w3.org/2001/XMLSchema#string> }}";
+
+        public IEnumerable<dynamic> Types
+        {
+            get { return _dynamicStore.BindObjectsWithSparql(TypesQuery); }
+        }
+
+        public dynamic AssertType(string name, IEnumerable<string> instanceProperties)
+        {
+            // check if one exists already
+            var type = _dynamicStore.BindObjectsWithSparql(String.Format(GetTypeByName, name)).FirstOrDefault();
+            if (type != null)
+            {
+                // update instance properties
+                type.ds__properties = instanceProperties;
+            } else
+            {
+                // create new type with instance properties
+                type = _dynamicStore.MakeNewObject(UriPrefix);
+                type.ds__properties = instanceProperties;
+                type.ds__name = name;
+                type.ds__type = "type";
+            }
+            _dynamicStore.SaveChanges();
+
+            return type;
+        }
+
+        public dynamic GetType(string name)
+        {
+            return _dynamicStore.BindObjectsWithSparql(String.Format(GetTypeByName, name)).FirstOrDefault();           
+        }
+
+        public void DeleteObject(string id)
+        {
+            var dataObjectContext = BrightstarService.GetDataObjectContext(_connectionString + ";storename=" + _storeId);
+            var store = dataObjectContext.OpenStore(_storeId);
+            store.GetDataObject(id).Delete();
+            store.SaveChanges();
+        }
+
+        /// <summary>
+        /// Creates a new persistent but untyped dynamic object.
+        /// </summary>
+        /// <returns>A new dynamic object</returns>
+        public dynamic CreateNewDynamicObject()
+        {
+            return _dynamicStore.MakeNewObject(UriPrefix);
+        }
+
+        /// <summary>
+        /// Creates a new persistent and typed dynamic object.
+        /// </summary>
+        /// <returns>A new dynamic object</returns>
+        public dynamic CreateNewDynamicObject(string typeName)
+        {
+            var t = GetType(typeName);
+            if (t == null) throw new Exception("Type does not exist");
+
+            var d = _dynamicStore.MakeNewObject(UriPrefix);
+            d.ds__type = t;
+            return d;
+        }
+
+        public void SaveChanges()
+        {
+            _dynamicStore.SaveChanges();
+        }
+    }
+}
